778d1f7f81a92c9d9208d44c65b118e71e71dc7f
Rob <rob.clark56+github@gmail.com>
2018-05-12 21:20:36 +0800

    Create StoreCheckout.php

---
+++ resources/StoreCheckout.php
@@ -0,0 +1,73 @@
+<?php
+/*
+  Example eCommerce checkout page for testing LightningPay
+*/
+
+define('CURRENCY','USD');
+$testnet = $_GET['testnet'] > 0;
+$chain_name = $testnet?'Testnet':'Mainnet';
+$order_id   = 42;
+$orders[$order_id]['products'][] = 
+		array(
+			'qty' =>1,
+			'price_ea' => 10.00,
+			'desc' =>'Pan Galactic Gargle Blaster'
+		);
+$orders[$order_id]['products'][] = 
+		array(
+			'qty' =>2,
+			'price_ea' =>5.00,
+			'desc' =>'Book of Vogon Poetry'
+		);
+$orders[$order_id]['products'][] = 
+		array(
+			'qty' =>3,
+			'price_ea' =>20.00,
+			'desc' =>'HODL Teeshirts'
+		);
+?>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Store Checkout</title>
+<style> body { background-color: <?php echo $testnet?'CornflowerBlue ':'SkyBlue';?>;} </style>
+</head>
+
+<body>
+<table>
+<td><img src="https://github.com/lightningnetwork/lnd/raw/master/logo.png"  width="150" height="150">
+<td><img src="https://image.freepik.com/free-icon/store_318-49896.jpg" width="150" height="150">
+</table>
+<h1>All Electric <?php echo $chain_name?> Emporium</h1>
+
+<h2>Your Shopping Cart - Order <?php echo $order_id?></h2>
+<?php //echo "<p><b>Current Exchange Rate:</b> 1 BTC = $ExchRate .</p>";?>
+<table border='1'>
+ <tr><th>Qty<th>Description<th>Price each<th>Price Total<br><?php echo CURRENCY;?></tr>
+ <?php
+ $order = $orders[$order_id];
+ $grand_total = 0;
+ foreach($order['products'] as $product){
+  $qty   = $product['qty'];
+  $desc  = $product['desc'];
+  $price = number_format($product['price_ea'],2);
+  $total = number_format($product['qty'] * $product['price_ea'],2);
+  $grand_total += $total;
+  echo "<tr><td>$qty<td>$desc<td align='right'>$ $price<td align='right'>$ $total</tr>";
+ }
+ echo '<tr><th colspan="3" align="right">Total<th align="right">$ '.number_format($grand_total,2).'</tr>';
+ echo '<tr><th colspan="3" align="right">&nbsp;<td>'; 
+ ?>
+ <form action="lightningPay.php<?php echo $testnet?'?testnet=1':'';?>" method="post"> 
+  <input type="hidden" name="memo" value="Order <?php echo $order_id;?>">  
+  <input type="hidden" name="amount" value="<?php echo $grand_total;?>">
+  <input type="hidden" name="currency" value="<?php echo CURRENCY;?>"> 
+  <input type="hidden" name="amount_format" value="<?php echo '$'.number_format($grand_total,2);?>">
+  <button type="submit">Pay Now</button><br>
+</form>
+ <?php  echo '</tr>';?>
+</table>
+
+</body>
+</html>
