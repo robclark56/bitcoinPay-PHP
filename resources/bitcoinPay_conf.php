39c37272d351f54bee382c1888d56ce5195e9a02
Rob <rob.clark56+github@gmail.com>
2018-06-12 13:07:47 +0800

    Update bitcoinPay_conf.php

--- resources/bitcoinPay_conf.php
+++ resources/bitcoinPay_conf.php
@@ -18,6 +18,7 @@ define('MINE_SECONDS'   ,'10800');	//e.g. 3 hours. Should be larger than EXPIRY_
 define('WORKING_DIRECTORY','/home/user/public_html/bitcoinPay');	//Used in cron mode.
 
 // Extended Public Keys
+// See https://github.com/robclark56/bitcoinPay-PHP/blob/master/README.md#1-get-your-xpub--tpub
 //  One line per wallet
 //  No limit to number of wallets, but xpub for DEFAULT_WALLET must exist
 $xpub['wallet_mainnet'] = 'xpub.....1Dwb';
@@ -27,7 +28,7 @@ $xpub['wallet_testnet'] = 'tpub.....7RkP';
 define('DEFAULT_WALLET'   ,'wallet_testnet');
 
 // eStore PrivateKey. Used to sign callback messages to the eStore, after payment has been confirmed.
-// See https://github.com/robclark56/bitcoinPay-PHP/blob/master/README.md#1-generate-privatepublic-key-pair
+// See https://github.com/robclark56/bitcoinPay-PHP/blob/master/README.md#2-generate-privatepublic-key-pair
 define('ESTORE_PRIV_KEY',
 '-----BEGIN RSA PRIVATE KEY-----
 [... lines deleted ...]
@@ -36,7 +37,7 @@ define('ESTORE_PRIV_KEY',
 );
 
 // Database values
-// See https://github.com/robclark56/bitcoinPay-PHP/blob/master/README.md#2-create-sql-database
+// See https://github.com/robclark56/bitcoinPay-PHP/blob/master/README.md#3-create-sql-database
 define('DB_USER','xxxxx');
 define('DB_PASS','xxxxx');	
 define('DB_HOST','xxxxx');			
